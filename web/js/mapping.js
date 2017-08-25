b0566474aacf22558195d89e59754fc4477e7762
pgngp <pgngp99@gmail.com>
2017-08-25 15:18:24 -0400

    When infowindow is opened, navigation will be disabled.

--- web/js/mapping.js
+++ web/js/mapping.js
@@ -8,14 +8,15 @@ function initMap()
         lat : 40.7831,
         lng : -73.9712
     };
+    
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom : 15,
         center : manhattan
     });
+    
     $.get("http://192.168.56.101:3000/properties").then(
             function(data) {
                 for (var i = 0; i < data.length; ++i) {
-                    console.log("lat: " + data[i].latitude);
                     var location = { 
                             lat : Number(data[i].latitude),
                             lng : Number(data[i].longitude)
@@ -25,6 +26,40 @@ function initMap()
                         position : location,
                         map : map
                     });
+                    
+                    var name = data[i].name;
+                    var address = data[i].address1;
+                    var marketId = data[i].marketId;
+                    
+                    var contentStr = "<div id='content'>" + 
+                        "Name: " + name + "<br>" + 
+                        "Address: " + address + "<br>" + 
+                        "Market: " + marketId + "<br>" + 
+                        "</div>";
+                    
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: contentStr
+                    });
+//                    map.setOptions({
+//                        gestureHandling: 'auto'
+//                    });
+                    google.maps.event.addListener(marker, 'click', (function(marker, contentStr) {
+                        return function() {
+                            infoWindow.setContent(contentStr);
+                            infoWindow.open(map, marker);
+                            map.setOptions({
+                                gestureHandling: 'none'
+                            });
+                        }
+                    })(marker, contentStr));
+                    
+                    google.maps.event.addListener(infoWindow, 'closeclick', (function(marker, contentStr) {
+                        return function() {
+                            map.setOptions({
+                                gestureHandling: 'auto'
+                            });
+                        }
+                    })(marker, contentStr));
                 }
                 
             },
